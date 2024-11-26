13a1c58250a387fb9c1af89fe1061438f825f54d
Iammujeeb45 <mujeeb.hardwin@gmail.com>
2024-11-26 18:59:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function addsum(s1, s2, s3, s4, s5, s6, name) {
   const total = s1 + s2 + s3 + s4 + s5 + s6;
-  const avg = total / 600;
+  const avg = total / 6;
   const percentage = avg * 100;
   return `The student ${name} Total marks ${total} avg is ${avg} and the Percentage ${percentage}%`;
 }
+
+// submit function
 
 function listenToclick(e) {
   e.preventDefault();
@@ -19,4 +21,28 @@ function listenToclick(e) {
 
   document.getElementById("temp").innerText = template;
 }
+
+// renter function
+
+// function resetfields() {
+//   document.getElementById("myForm").reset();
+//   document.getElementById("temp").innerText = "";
+  
+// }
 document.getElementById("myForm").addEventListener("submit", listenToclick);
+
+document.getElementById("resetButton").addEventListener("click", resetfields);
+
+
+function resetfields() {
+  
+  document.getElementById("s1").value = "";
+  document.getElementById("s2").value = "";
+  document.getElementById("s3").value = "";
+  document.getElementById("s4").value = "";
+  document.getElementById("s5").value = "";
+  document.getElementById("s6").value = "";
+  document.getElementById("myname").value = "";
+
+  document.getElementById("temp").innerText = ""; 
+}
